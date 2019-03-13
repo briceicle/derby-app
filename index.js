460d10c11a48541fc2ad364354e7de633d827fbb
Brice Nkengsa <bricenkengsa@gmail.com>
2019-03-13 12:20:17 -0400

    first working version

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-var derby = require('derby');
-var app = derby.createApp('directory', __filename);
-
+var app = module.exports = require('derby').createApp('derby-app', __filename);
 app.use(require('derby-debug'));
 app.serverUse(module, 'derby-stylus');
 app.use(require('d-bootstrap'));
 app.loadViews(__dirname + '/views');
 app.loadStyles(__dirname + '/styles');
-app.component(require('d-connection-alert'));
 app.component(require('d-before-unload'));
 
 app.get('/', function(page, model) {
@@ -89,5 +86,3 @@ EditForm.prototype.deletePerson = function() {
     app.history.back();
   });
 };
-
-module.exports = app;
